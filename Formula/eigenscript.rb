87e5eebe1e5758018458f1b68116905762546598
InauguralPhysicist <InauguralPhysicist@gmail.com>
2026-08-03 08:36:40 -0500

    eigenscript 0.35.2

--- Formula/eigenscript.rb
+++ Formula/eigenscript.rb
@@ -1,8 +1,8 @@
 class Eigenscript < Formula
   desc "Bytecode VM language with copy-and-patch JIT, observers, and temporal queries"
   homepage "https://github.com/InauguralSystems/EigenScript"
-  url "https://github.com/InauguralSystems/EigenScript/archive/refs/tags/v0.35.1.tar.gz"
-  sha256 "d8c347ac0b13fad7250d25287609307a6e0733dd26bdc7e9ac084a8c407621e8"
+  url "https://github.com/InauguralSystems/EigenScript/archive/refs/tags/v0.35.2.tar.gz"
+  sha256 "5742fe401d4348c7aa01c98365133f1468391f99e1b4ff69a347c159827db0d5"
   license "MIT"
   head "https://github.com/InauguralSystems/EigenScript.git", branch: "main"
 
